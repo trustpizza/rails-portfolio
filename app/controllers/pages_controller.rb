f08658b0ffaf6ed57a7520b8b3c4e98a0ac4c14f
Axel <maxelolsson@gmail.com>
2023-09-12 12:35:08 -0500

    update pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def bio
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/bio'
+  get 'pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "home#index"
   # Defines the root path route ("/")
